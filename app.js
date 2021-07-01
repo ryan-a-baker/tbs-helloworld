831624cf0cdb176b2c980efeb413d4b29670a345
Ryan Baker <ryan.andrew.baker@outlook.com>
2021-07-01 12:35:23 -0500

    Running

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const http = require('http')
 const port = process.env.PORT || 8080
 
-const requestHandler = (request, response) => {
-    response.end(`<!DOCTYPE html>
-<html>
-  <head>
-    <title>Powered By Paketo Buildpacks</title>
-  </head>
-  <body>
-    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
-  </body>
-</html>`)
-}
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is now listening on ${port}`)
-})
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
