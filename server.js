4eaf22b6bb73a923e0c88f5a57fe8d63eeeb3266
Ryan Baker <ryan.andrew.baker@outlook.com>
2021-07-01 15:37:43 -0500

    Demo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http')
+const uc = require('upper-case')
 const port = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(uc.upperCase("Hello World!"));
 });
 
 process.on('SIGINT', function() {
