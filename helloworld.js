3ba5d0786f928f587a78a53f6bf66180686615fa
Ryan Baker <ryan.andrew.baker@outlook.com>
2021-07-01 10:35:12 -0500

    Demo

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,24 @@
-var http = require('http');
-var uc = require('upper-case');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(uc.upperCase("Hello World!"));
-  res.end();
-}).listen(8080);
+const http = require('http')
+const port = process.env.PORT || 8080
+
+const requestHandler = (request, response) => {
+    response.end(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Powered By Paketo Buildpacks</title>
+  </head>
+  <body>
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
+  </body>
+</html>`)
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
